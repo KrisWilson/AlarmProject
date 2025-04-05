b459d578c6ff1c8a7e2655fab9a65877b171e6ce
KrisWilson <kris.wilson@op.pl>
2025-04-05 18:22:31 +0200

    making steps into lcd

--- src/util/lcd.cpp
+++ src/util/lcd.cpp
@@ -2,3 +2,14 @@
 // Konfiguracja LCD 16x2
 int backlightTime = 0; // czas podświetlenia ekranu
 LiquidCrystal_I2C lcd(0x3F,20,4);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+void wyswietl(
+        String _txt,    // określona wartość tekstu (max 16)
+        int pos = 0,    // górna (zero) albo dolna (jeden) linijka
+        int mode = 0    // tryb alignu tekstu, 0 - center, 1 - left, 2 - right
+        ){
+      lcd.setCursor((16-_txt.length())/2,pos);
+      for(int i=0;i<_txt.length())
+        lcd.write((_txt[i])
+
+}
